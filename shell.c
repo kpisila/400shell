9b0b77f451114e38320e318426bee9a6d04ecdfd
niall-healy <48368862+niall-healy@users.noreply.github.com>
2019-03-18 16:42:23 -0400

    Update shell.c

--- shell.c
+++ shell.c
@@ -7,7 +7,50 @@
 #include <sys/wait.h>
 #include <string.h>
 
-int main(){
-  printf("TEST");
-return 0;
+void shellLoop();
+char *readString();
+char **commandLineParser(char *string);
+void runCommand();
+
+int main()
+{
+  shellLoop();
+
+  return 0;
+}
+
+void shellLoop()
+{
+  char *string;
+  char **commands;
+
+  do
+  {
+    printf("~ ");
+    string = readString();
+
+    if(strcmp(string, "exit") == 0)
+    {
+      exit(0);
+    }
+
+    commands = commandLinePrser(string);
+
+
+  } while();
+}
+
+char *readString()
+{
+
+}
+
+char **commandLineParser(char *string)
+{
+
+}
+
+void runCommand()
+{
+  
 }
